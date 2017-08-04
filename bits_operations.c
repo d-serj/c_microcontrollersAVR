f7ebe61f1bf8d862abb9e96acb22f7af8f58f490
d-serj <sp11kenny@gmail.com>
2017-08-04 09:06:58 +0300

    Update bits_operations.c

--- bits_operations.c
+++ bits_operations.c
@@ -1,27 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void print_byte (char);
+
 int main ()
 {
     char a1 = 0;
     int i;
 
-    for(i=sizeof(a1)*8-1; i>=0; --i)
-		printf("%d",(a1 >> i)&1);
+    print_byte(a1);
 
-	printf("\n");
     a1 ^= (1 << 0);
+    print_byte(a1);
 
-
-    for(i=sizeof(a1)*8-1; i>=0; --i)
-		printf("%d",(a1 >> i)&1);
-
-	printf("\n");
     a1 &= ~(1 << 0);
-
-	for(i=sizeof(a1)*8-1; i>=0; --i)
-	printf("%d",(a1 >> i)&1);
-
+    print_byte(a1);
 
     return 0;
+}
+
+void print_byte (char input)
+{
+    int i;
+
+    for(i=sizeof(input)*8-1; i>=0; --i)
+		printf("%d",(input >> i)&1);
+
+    putchar('\n');
 }
